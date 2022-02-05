6c4fba1060c5722b1d168bc250515efdc676a5d8
Maxim Pupykin <58301278+Finnyke@users.noreply.github.com>
2022-02-05 17:51:40 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,33 +4,35 @@
 
 using namespace std;
 
-char* extractBitsToChar(long* amount, const char* filename) {
+char* extractBitsToChar(long long* amount, const char* filename) {
 	ifstream in(filename, ios::binary);
 	if (!in.is_open()) {
 		cerr << "File " << filename << " could not have been opened" << endl;
 	}
 	cout << "Extracting bits... ";
-	long N = 0;
-	char num[4]{0};
-	in.read(num, 4 * sizeof(char));
-	N |= (static_cast<unsigned char>(num[3]) << 24);
-	N |= (static_cast<unsigned char>(num[2]) << 16);
-	N |= (static_cast<unsigned char>(num[1]) << 8);
-	N |= (static_cast<unsigned char>(num[0]) << 0);
+	long long N = 0;
+	char num[8]{0};
+	in.read(num, 8 * sizeof(char));
+	for (int i = 7; i >= 0; --i) {
+		long long temp = 0;
+		temp |= static_cast<unsigned char>(num[i]);
+		temp = temp << i * 8;
+		N |= temp;
+	}
 	char* bits = new char[N];
 	char* buffer = new char[N / 8 + 1];
 	long cnt = 0;
 	in.read(buffer, (N / 8 + 1) * sizeof(char));
-	for (long i = 0; i < N / 8; ++i) {
+	for (long long i = 0; i < N / 8; ++i) {
 		unsigned char c = static_cast<unsigned char>(buffer[i]);
-		for (long j = 0; j < 8; ++j) {
+		for (long long j = 0; j < 8; ++j) {
 			if (c & 0b10000000) bits[i * 8 + j] = '1';
 			else bits[i * 8 + j] = '0';
 			c = c << 1;
 		}
 	}
 	unsigned char c = static_cast<unsigned char>(buffer[N / 8]);
-	for (long j = 0; j < N % 8; ++j) {
+	for (long long j = 0; j < N % 8; ++j) {
 		if (c & 0b10000000) bits[N - N % 8 + j] = '1';
 		else bits[N - N % 8 + j] = '0';
 		c = c << 1;
@@ -51,8 +53,8 @@ int main(int argc, char** argv) {
 		cerr << "File" << argv[1] << " could not have been opened" << endl;
 		return 1;
 	}
-	long N;
-	N = atol(argv[2]);
+	long long N;
+	N = atoll(argv[2]);
 	if (N <= 0) {
 		cerr << "Invalid number of bits" << endl;
 		return 1;
@@ -72,7 +74,7 @@ int main(int argc, char** argv) {
 	unsigned short cnt = 0;
 	out.write(reinterpret_cast<const char*>(&N), sizeof(N));
 	if (p < 1 && p > 0) {
-		for (long i = 0; i < N + 8; ++i) {
+		for (long long i = 0; i < N + N % 8; ++i) {
 			double r = round(((double)rand() / (RAND_MAX)) - 0.5 + p);
 			++cnt;
 			if (r) buffer |= (1 << (8 - cnt));
@@ -85,10 +87,11 @@ int main(int argc, char** argv) {
 	}
 	else {
 		p ? buffer = 0b11111111 : buffer = 0b00000000;
-		for (long i = 0; i < N / 8 + 1; ++i) {
+		for (long long i = 0; i < N / 8 + 1; ++i) {
 			out.put(buffer);
 		}
 	}
+	cout << endl;
 	cout << N << " bits successfully written to " << argv[1] << endl;
 	out.close();
 
