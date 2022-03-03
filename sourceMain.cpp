9c07f41d83e51032d3a737daff82cfb53a79754d
Maxim Pupykin <58301278+Finnyke@users.noreply.github.com>
2022-03-03 19:33:00 +0400

    Update sourceMain.cpp

--- sourceMain.cpp
+++ sourceMain.cpp
@@ -5,109 +5,55 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
+	if (argc != 3 && argc != 4) {
 		cerr << "Invalid argument amount" << endl;
 		return 1;
 	}
-	ifstream in(argv[1], ios::binary);
-	if (!in.is_open()) {
+	ofstream out(argv[1], ios::binary);
+	if (!out.is_open()) {
 		cerr << "File" << argv[1] << " could not have been opened" << endl;
 		return 1;
 	}
-	ofstream out(argv[2], ios::binary);
-	if (!out.is_open()) {
-		cerr << "File" << argv[2] << " could not have been opened" << endl;
+	long long N;
+	N = atoll(argv[2]);
+	if (N <= 0) {
+		cerr << "Invalid number of bits" << endl;
 		return 1;
 	}
-	long long N = 0;
-	char num[8]{ 0 };
-	in.read(num, 8 * sizeof(char));
-	for (int i = 7; i >= 0; --i) {
-		long long temp = 0;
-		temp |= static_cast<unsigned char>(num[i]);
-		temp <<= i * 8;
-		N |= temp;
+	double p;
+	if (argc == 4) {
+		p = atof(argv[3]);
+		if (p < 0 || p > 1) {
+			cerr << "Invalid probability" << endl;
+			return 1;
+		}
 	}
+	else p = 0.5;
+	cout << "Writing... ";
+	srand(static_cast<unsigned>(time(nullptr)));
 	out.write(reinterpret_cast<const char*>(&N), sizeof(N));
-	cout << "Encoding... ";
-	{
-		long long output;
-		unsigned long long cnt;
-		char buffer[7]{ 0 };
-		char c;
-		char* bits = new char[4];
-		for (auto i = 0ll; i < (N / 8 + 1) / 4; ++i) {
-			output = 0;
-			cnt = 0x8000000000000000;
-			in.read(bits, 4);
-			for (auto j = 0u; j < 4; ++j) {
-				c = static_cast<unsigned char>(bits[j]);
-				for (auto k = 0u; k < 4; ++k) {
-					if (c & 0b10000000) buffer[k] = 1;
-					else buffer[k] = 0;
-					c <<= 1;
-				}
-				buffer[4] = buffer[0] ^ buffer[1] ^ buffer[2];
-				buffer[5] = buffer[1] ^ buffer[2] ^ buffer[3];
-				buffer[6] = buffer[0] ^ buffer[2] ^ buffer[3];
-				for (auto k = 0u; k < 7; ++k, cnt >>= 1) {
-					if (buffer[k]) output |= cnt;
-				}
-				for (auto k = 0u; k < 4; ++k) {
-					if (c & 0b10000000) buffer[k] = 1;
-					else buffer[k] = 0;
-					c <<= 1;
-				}
-				buffer[4] = buffer[0] ^ buffer[1] ^ buffer[2];
-				buffer[5] = buffer[1] ^ buffer[2] ^ buffer[3];
-				buffer[6] = buffer[0] ^ buffer[2] ^ buffer[3];
-				for (auto k = 0u; k < 7; ++k, cnt >>= 1) {
-					if (buffer[k]) output |= cnt;
-				}
-			}
-			const char* buf = reinterpret_cast<const char*>(&output);
-			for (auto j = 7u; j > 0; --j) {
-				out.put(buf[j]);
+	if (p < 1 && p > 0) {
+		unsigned char buffer = 0b00000000;
+		unsigned short cnt = 0b10000000;
+		for (long long i = 0; i < N + 8 - N % 8; ++i) {
+			if (rand() < RAND_MAX * p) buffer |= cnt;
+			cnt >>= 1;
+			if (!cnt) {
+				cnt = 0b10000000;
+				out.put(buffer);
+				buffer = 0b00000000;
 			}
 		}
-		if ((N / 8 + 1) % 4 > 0) {
-			output = 0;
-			cnt = 0x8000000000000000;
-			in.read(bits, (N / 8 + 1) % 4);
-			for (auto j = 0u; j < (N / 8 + 1) % 4; ++j) {
-				c = static_cast<unsigned char>(bits[j]);
-				for (auto k = 0u; k < 4; ++k) {
-					if (c & 0b10000000) buffer[k] = 1;
-					else buffer[k] = 0;
-					c <<= 1;
-				}
-				buffer[4] = buffer[0] ^ buffer[1] ^ buffer[2];
-				buffer[5] = buffer[1] ^ buffer[2] ^ buffer[3];
-				buffer[6] = buffer[0] ^ buffer[2] ^ buffer[3];
-				for (auto k = 0u; k < 7; ++k, cnt >>= 1) {
-					if (buffer[k]) output |= cnt;
-				}
-				for (auto k = 0u; k < 4; ++k) {
-					if (c & 0b10000000) buffer[k] = 1;
-					else buffer[k] = 0;
-					c <<= 1;
-				}
-				buffer[4] = buffer[0] ^ buffer[1] ^ buffer[2];
-				buffer[5] = buffer[1] ^ buffer[2] ^ buffer[3];
-				buffer[6] = buffer[0] ^ buffer[2] ^ buffer[3];
-				for (auto k = 0u; k < 7; ++k, cnt >>= 1) {
-					if (buffer[k]) output |= cnt;
-				}
-			}
-			const char* buf = reinterpret_cast<const char*>(&output);
-			for (auto j = 7u; j > 0; --j) {
-				out.put(buf[j]);
-			}
-		}
-		delete[] bits;
 	}
-	cout << "Encoding completed successfully" << endl;
-	in.close();
+	else {
+		unsigned char buffer;
+		p ? buffer = 0b11111111 : buffer = 0b00000000;
+		for (long long i = 0; i < N / 8 + 1; ++i) {
+			out.put(buffer);
+		}
+	}
+	cout << N << " bits successfully written to " << argv[1] << endl;
 	out.close();
+
 	return 0;
 }
